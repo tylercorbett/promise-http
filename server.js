8d52fdc57fc5d10e66aaf9f4d97f3f0c056a10be
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-15 15:13:08 -0800

    testing works

--- server.js
+++ server.js
@@ -1,39 +1,6 @@
 const http = require('http');
-const { parse } = require('url');
+const app = require('./lib/app');
 
-http.createServer((req, res) => {
-  const url = parse(req.url);
-  let message = '';
-
-  if(url.pathname === '/birthday') {
-    message = 'happy birthday';
-  }
-  else if(url.pathname === '/tomorrow') {
-    message = 'tomorrow for sure';
-  }
-  else if(url.pathname === '/birthday/tomorrow') {
-    message = 'happy birthday tomorrow';
-  }
-  else {
-    message = '404 where did the page go?';
-  }
-
-  res.setHeader('Content-Type', 'text/html');
-
-  res.end(`
-  <html>
-    <head>
-        <title>
-            Testing
-        </title>
-    </head>
-    <body>
-        <h1 style="color:purple">
-            ${message}
-        </h1>
-    </body>
-  </html>
-  `);
-})
+http.createServer(app)
   .listen(7890);
 
