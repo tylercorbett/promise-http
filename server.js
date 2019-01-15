37f49c9c680a07d258d7e2d797d49b950855c768
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-15 14:59:53 -0800

    changing routes changes message on screen

--- server.js
+++ server.js
@@ -3,12 +3,23 @@ const { parse } = require('url');
 
 http.createServer((req, res) => {
   const url = parse(req.url);
+  let message = '';
+
   if(url.pathname === '/birthday') {
-    console.log('it worked'); 
+    message = 'happy birthday';
+  }
+  else if(url.pathname === '/tomorrow') {
+    message = 'tomorrow for sure';
+  }
+  else if(url.pathname === '/birthday/tomorrow') {
+    message = 'happy birthday tomorrow';
+  }
+  else {
+    message = '404 where did the page go?';
   }
 
-  res.setHeader('Content-Type', 'text/html')
-  res.end('<html><head><title>Testing</title></head><body><h1>Thanks for visiting!</h1></body></html>');
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`<html><head><title>Testing</title></head><body><h1>${message}</h1></body></html>`);
 })
   .listen(7890);
 
