df2faeea04aac80db97045fe62941b1040173923
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-15 17:33:11 -0800

    rick and morty api working with test

--- test/services/rickAndMortyApi.test.js
+++ test/services/rickAndMortyApi.test.js
@@ -1,11 +1,11 @@
 const getCharacter = require('../../lib/services/rickAndMortyApi');
 
 describe('rick and morty api tests', () => {
-  it.skip('gets a character by id', () => {
+  it('gets a character by id', () => {
     return getCharacter(1)
       .then(character => {
         expect(character).toEqual({
-          name: 'Rick Sanche',
+          name: 'Rick Sanchez',
           status: 'Alive',
           species: 'Human'
         });
